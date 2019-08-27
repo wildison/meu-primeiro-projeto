72079bdc049efc0fcd77a85db2e8f6b7379f69a8
Wildison Marcelino <biloke157@hotmail>
2019-08-26 21:07:24 -0300

    Versao 0.0.1

---
+++ src/main/java/com/WildisonMarcelino/RPG/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.WildisonMarcelino.RPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+}
